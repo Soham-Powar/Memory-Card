e17e05089de8634d409f69eab67f5b891b111906
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-12 21:32:56 +0530

    Set the id returned by API as the key of GameCard.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -16,7 +16,10 @@ export default function GameBoard() {
 	return (
 		<div className="game-board grid-cols-4 grid gap-3 p-4 bg-pink-200">
 			{imageURLs.map((url, index) => {
-				return <GameCard key={index} url={url} index={index} />
+				//get the id of the pokemon (returned by api)
+				const number = url.split('/').pop().split('.')[0];
+
+				return <GameCard key={number} url={url} index={index} />
 			})}
 		</div>
 	);
