03ce3bf41b542b14f12628905e3d3d1ae5ab11e6
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-13 11:09:20 +0530

    Add score and bestScore props.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import GameCard from "./GameCard.jsx";
+import ScoreBoard from "./ScoreBoard.jsx";
 
 import fetchData from '../services/fetchData.js'
 import shuffleArray from "../services/shuffleArray.js";
@@ -9,6 +10,8 @@ import shuffleArray from "../services/shuffleArray.js";
 
 export default function GameBoard() {
 	const [imageURLs, setImageURLs] = useState([]);
+	const [score, setScore] = useState(0);
+	const [bestScore, setBestScore] = useState(0);
 
 	useEffect(() => {
 		(async () => {
@@ -23,14 +26,19 @@ export default function GameBoard() {
 	}
 
 	return (
-		<div className="game-board grid-cols-4 grid gap-3 p-4 bg-pink-200">
-			{imageURLs.map((url) => {
-				//get the id of the pokemon (returned by api)
-				const number = url.split('/').pop().split('.')[0];
+		<>
+			<div>
+				<ScoreBoard score={score} bestScore={bestScore} />
+			</div>
+			<div className="game-board grid-cols-4 grid gap-3 p-4 bg-pink-200">
+				{imageURLs.map((url) => {
+					//get the id of the pokemon (returned by api)
+					const number = url.split('/').pop().split('.')[0];
 
-				return <GameCard key={number} url={url} id={number} onFirstClick={handleFirstClick} />
-			})}
-		</div>
+					return <GameCard key={number} url={url} id={number} onFirstClick={handleFirstClick} setScore={setScore} setBestScore={setBestScore} />
+				})}
+			</div>
+		</>
 	);
 
 }
