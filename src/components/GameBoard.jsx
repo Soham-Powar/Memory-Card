95186377d8b8106e7302330ede23e84a8ce3637b
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-13 11:41:34 +0530

    Add resetSignal to GameBoard.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -13,32 +13,38 @@ export default function GameBoard() {
 	const [imageURLs, setImageURLs] = useState([]);
 	const [score, setScore] = useState(0);
 	const [bestScore, setBestScore] = useState(0);
+	const [resetSignal, setResetSignal] = useState(false);
+
+	useEffect(() => {
+		fetchImages();
+	}, []);
 
 	async function fetchImages() {
 		const data = await fetchData();
 		setImageURLs(data);
 	}
 
-	useEffect(() => {
-		fetchImages();
-	}, []);
 
 	function handleFirstClick() {
 		setImageURLs((prev) => shuffleArray(prev));
+	}
+
+	function resetAllCards() {
+		setResetSignal(false);
 	}
 
 	return (
 		<>
 			<div>
 				<ScoreBoard score={score} bestScore={bestScore} />
-				<ResetBtn fetchImages={fetchImages} setScore={setScore} />
+				<ResetBtn fetchImages={fetchImages} setScore={setScore} resetAllCards={resetAllCards} />
 			</div>
 			<div className="game-board grid-cols-4 grid gap-3 p-4 bg-pink-200">
 				{imageURLs.map((url) => {
 					//get the id of the pokemon (returned by api) to use it as key.
 					const number = url.split('/').pop().split('.')[0];
 
-					return <GameCard key={number} url={url} id={number} onFirstClick={handleFirstClick} setScore={setScore} setBestScore={setBestScore} score={score} bestScore={bestScore} />
+					return <GameCard key={number} url={url} id={number} onFirstClick={handleFirstClick} setScore={setScore} setBestScore={setBestScore} score={score} bestScore={bestScore} resetSignal={resetSignal} />
 				})}
 			</div>
 		</>
