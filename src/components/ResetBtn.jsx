cffe79fc5a6702f920ec0ee93bbc9968d7f4c819
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-13 11:42:21 +0530

    Add function to resetAllCards on button click.

--- src/components/ResetBtn.jsx
+++ src/components/ResetBtn.jsx
@@ -1,6 +1,7 @@
-export default function ResetBtn({ fetchImages, setScore }) {
+export default function ResetBtn({ fetchImages, setScore, resetAllCards }) {
 	return (
 		<button onClick={() => {
+			resetAllCards();
 			fetchImages();
 			setScore(0);
 		}}>Reset</button>
