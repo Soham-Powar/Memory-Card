51bfa6ee907ab65f9a022a734f41251e30b47daa
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-14 20:43:25 +0530

    Change scoreBoard text.

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -2,10 +2,10 @@ export default function ScoreBoard({ score, bestScore }) {
 	return (
 		<div className="">
 			<div className="text-3xl font-semibold text-gray-800">
-				Score is {score}
+				Score: {score}
 			</div>
 			<div className="text-xl text-gray-600">
-				Best score is {bestScore}
+				Your Personal Best: {bestScore}
 			</div>
 		</div>
 	);
